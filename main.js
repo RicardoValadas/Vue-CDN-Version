669381b7e866142c9fe06f44c9863fd5a40974d4
Ricardo <ricardo.valadas07@gmail.com>
2021-10-14 10:49:15 +0200

    update

--- main.js
+++ main.js
@@ -5,8 +5,20 @@ const app = Vue.createApp({
             welcome: 'Hello',
             name: '',
             labelName: 'Insert your name',
+            images: [],
+            source: 'https://picsum.photos/v2/list'
         }
     },
+    mounted() {
+        fetch('https://picsum.photos/v2/list')
+            .then(res => res.json())
+            .then(data => this.images = data)
+            .catch(err => console.log(err.message))
+    }
 })
 
 app.mount('#main')
+console.log({ app })
+
+
+
